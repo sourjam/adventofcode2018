dabe402e77a08ba5dfc134d8991c0daa4fcade7a
Jim Yang <jim@homerlearning.com>
2018-12-06 22:52:58 -0500

    day 2 part 2 solved

--- 2/2.js
+++ 2/2.js
@@ -28,3 +28,34 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
   });
   console.log('Day 2 - Part 1 answer is: ', twoLetterCount * threeLetterCount)
 });
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  const idArr = data.split('\n');
+  let foundAnswer = false;
+
+  for(let i = 0; i < idArr.length - 1; i++) {
+    let base = idArr[i];
+
+    for(let j = 1; j < idArr.length - 1; j++) {
+      let compare = idArr[j];
+      let diff = 0;
+      let indices = [];
+      for (let k = 0; k < base.length - 1; k++) {
+        if (base[k] === compare[k]) {
+          // do nothing
+        } else {
+          indices.push(k)
+          diff += 1;
+        }
+      }
+      if (diff === 1 && !foundAnswer) {
+        foundAnswer = true;
+        let index = indices[0];
+        let first = base.slice(0, index)
+        let second = base.slice(index + 1)
+        console.log('Day 2 - Part 2 answer is: ', first + second)
+      }
+    }
+  }
+
+});
