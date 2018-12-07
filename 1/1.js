cef802a024a1fcb70fd73c80dc256885430a2e99
Jim Yang <jim@homerlearning.com>
2018-12-06 21:32:56 -0500

    day 1 part 2 answered

--- 1/1.js
+++ 1/1.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 fs.readFile('input.txt', 'utf8', (err, data) => {
   const valueArr = data.split('\n');
   let baseValue = 0;
+  let freqMap = {};
+  let foundDupe = false;
   valueArr.forEach((value, index) => {
     const num = Number(value.slice(1));
     if (value[0] === '+') {
@@ -11,5 +13,29 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
       baseValue -= num;
     }
   });
-  console.log('Day 1 answer is: ', baseValue);
+  console.log('Day 1 - Part 1 answer is: ', baseValue);
+});
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  const valueArr = data.split('\n');
+  let baseValue = 0;
+  let freqMap = {};
+  let foundDupe = false;
+  do {
+    valueArr.forEach((value, index) => {
+      if (value === '') return;
+      const num = Number(value.slice(1));
+      if (value[0] === '+') {
+        baseValue += num
+      } else {
+        baseValue -= num;
+      }
+      if (freqMap[baseValue] !== baseValue) {
+        freqMap[baseValue] = baseValue;
+      } else if (freqMap[baseValue] === baseValue && foundDupe === false) {
+        foundDupe = baseValue;
+      }
+    });
+  } while (foundDupe === false);
+  console.log('Day 1 - Part 2 answer is: ', foundDupe);
 });
